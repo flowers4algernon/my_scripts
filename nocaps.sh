53b9e97a8e794b66b4704355e03c3a36ec78ddca
Geoff Wark <gtw@airmail.cc>
2017-06-27 16:56:50 -0400

    Initialize repository

---
+++ nocaps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Make Caps Lock a Control key.
+# NOTE: This should be layout ambiguous.
+
+setxkbmap -layout "$(setxkbmap -print | awk -F + '/xkb_symbols/ {print $2}')" -option ctrl:nocaps
